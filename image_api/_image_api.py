367aefdca2896b93ff51521a68cd467caf9e5e7d
kj <94023052+lkaijie@users.noreply.github.com>
2024-04-06 01:03:23 -0600

    feat: added format/w/h fields

--- image_api/_image_api.py
+++ image_api/_image_api.py
@@ -2,6 +2,7 @@ import json
 from image_api._base import _Base
 import image_api.config as config
 # from _image_item import ImageItem
+# from image_api._image_item import ImageItem
 from image_api._image_item import ImageItem
 
 # low = 640 x 420
@@ -11,46 +12,65 @@ from image_api._image_item import ImageItem
 
 class ImageApi(_Base):
     
+    # Image Quality
+    quality_map = {
+        "low": " 640x420p",
+        "mid": " 1280x720p",
+        "high": " 1920x1080p"
+    }
+
+    
     def __init__(self, timeout: int) -> None:
         super().__init__(timeout)
         self.image_endpoint = config.IMAGE_ENDPOINT
+        
     # hidden methods here
     def _extract_images(self, soup) -> dict:
         pass
 
     def search(self, query, quality = "" , count = 10, width: int = None, height: int = None) -> dict:
-        # handling custom image quality here
-        if quality == "low":
-            quality = " 640x420"
-        elif quality == "mid":
-            quality = " 1280x720"
-        elif quality == "high":
-            quality = " 1920x1080"
+        # handle image quality
+        quality = self.quality_map.get(quality, "")
         
         if width and height:
-            quality = f"{width}x{height}"
+            quality = f"{width}x{height}p"
         print(self.image_endpoint + query + quality)
         soup = self._parse_url(self.image_endpoint + query + quality)
 
         # this is every single item li
         # imgs = []
         imgs: list[ImageItem] = []
+        
         for item in soup.find_all("li", attrs={"data-idx": True}):
-            item = item.find("a", attrs={"class": "iusc"})
+            # testing = item
+            # print(testing)
+            img_item = item.find("a", attrs={"class": "iusc"})
+            name_layer = item.find("a", attrs={"class": "inflnk"})
+            # img_dimensions = img_item.find("span", attrs={"class": "nowrap"}).text.split("·")
+            img_dimensions = item.find("span", attrs={"class": "nowrap"}).text.split("·")
             img_url = "null"
-            img_item = ImageItem()
-            if item:
+            img_dict = {}
+            if img_item:
                 try:
-                    img_url = json.loads(item["m"])
-                    img_url = img_url["murl"]
-                    img_item.url = img_url
-                    
-                except KeyError:
+                    img_name = name_layer["aria-label"]
+                    w_h = img_dimensions[0].split("x")
+                    img_width = w_h[0].strip()
+                    img_height = w_h[1].strip()
+                    img_format = img_dimensions[1].strip()
+                    img_url = json.loads(img_item["m"])
+                    # with open("liitem.html", "w") as file:
+                    #     file.write(item.prettify())
 
+                    img_dict['url'] = img_url["murl"]
+                    img_dict['title'] = img_name
+                    img_dict['width'] = img_width
+                    img_dict['height'] = img_height
+                    # img_format = img_format
+                    img_dict['format'] = img_format
+                except KeyError:
                     pass
-            # imgs.append(img_url)
-            imgs.append(img_item)
-        # return imgs[:count]
+            if img_url != "null":
+                imgs.append(ImageItem(img_dict))
         return imgs
             
         
--- image_api/_image_item.py
+++ image_api/_image_item.py
@@ -1,7 +1,18 @@
-class Image:
-    def __init__(self, url, title = "", source = "", width = "", height = ""):
-        self.url = url
-        self.title = title
-        self.source = source
-        self.width = width
-        self.height = height
+class ImageItem:
+    # def __init__(self, url, title = "", source = "", width = "", height = ""):
+    #     self.url = url
+    #     self.title = title
+    #     self.source = source
+    #     self.width = width
+    #     self.height = height
+    def __init__(self, dict: dict) -> None:
+        print(dict)
+        try:
+            self.url = dict["url"]
+            self.title = dict["title"]
+            self.width = dict["width"]
+            self.height = dict["height"]
+            self.format = dict["format"]
+        except KeyError:
+            pass
+        pass
