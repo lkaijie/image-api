c8dcb0e7f5566e8c3b44ab2e7d9c1fcca8ac83bb
kj <94023052+lkaijie@users.noreply.github.com>
2024-04-07 16:58:31 -0600

    refactor: cleaned code

--- image_api/_image_api.py
+++ image_api/_image_api.py
@@ -28,13 +28,24 @@ class ImageApi(_Base):
     def _extract_images(self, soup) -> dict:
         pass
 
-    def search(self, query, quality = "" , count = 10, width: int = None, height: int = None) -> dict:
+    def search(self, query, quality = "" , count = 10, width: int = None, height: int = None) -> list[ImageItem]:
+        """Search for images
+
+        Args:
+            query (str): search query
+            quality (str, optional): Options (low, medium, high). Defaults to "".
+            count (int, optional): Count of image to return (does not affect speed). Defaults to 10.
+            width (int, optional): Custom width search. Defaults to None.
+            height (int, optional): Custom height search. Defaults to None.
+
+        Returns:
+            list[ImageItem]: Returns a list of ``ImageItemDict``
+        """
         # handle image quality
         quality = self.quality_map.get(quality, "")
         
         if width and height:
             quality = f"{width}x{height}p"
-        print(self.image_endpoint + query + quality)
         soup = self._parse_url(self.image_endpoint + query + quality)
 
         # this is every single item li
@@ -42,60 +53,46 @@ class ImageApi(_Base):
         imgs: list[ImageItem] = []
         
         for item in soup.find_all("li", attrs={"data-idx": True}):
-            # testing = item
-            # print(testing)
             img_item = item.find("a", attrs={"class": "iusc"})
             name_layer = item.find("a", attrs={"class": "inflnk"})
-            # img_dimensions = img_item.find("span", attrs={"class": "nowrap"}).text.split("·")
-            img_dimensions = item.find("span", attrs={"class": "nowrap"}).text.split("·")
-            img_url = "null"
+            img_dimensions = item.find("span", attrs={"class": "nowrap"})
+            
+            img_dict = {}
+            img_url = {}
+            img_url['murl'] = "null"
             img_width = "null"
             img_height = "null"
             img_format = "null"
             img_title = "null"
-            img_dict = {}
 
             if img_item:
                 img_url = json.loads(img_item["m"])
             
             if img_dimensions:
+                img_dimensions = img_dimensions.text.split("·")
                 img_width, img_height = img_dimensions[0].split("x")
                 img_format = img_dimensions[1].strip()
                 
             if name_layer:
                 img_title = name_layer["aria-label"]
                 
+            # img_dict['url'] = img_url["murl"]
+            # img_dict['title'] = img_title
+            # img_dict['width'] = img_width
+            # img_dict['height'] = img_height
+            # img_dict['format'] = "." + img_format
+            img_dict = {
+                'url': img_url["murl"],
+                'title': img_title,
+                'width': img_width,
+                'height': img_height,
+                'format': "." + img_format
+            }
             
-            img_dict['url'] = img_url["murl"]
-            img_dict['title'] = img_title
-            img_dict['width'] = img_width
-            img_dict['height'] = img_height
-            img_dict['format'] = img_format
             imgs.append(ImageItem(img_dict))
-        return imgs
+        return imgs[:count]
             
-            
-            if img_item:
-                try:
-                    img_name = name_layer["aria-label"]
-                    w_h = img_dimensions[0].split("x")
-                    img_width = w_h[0].strip()
-                    img_height = w_h[1].strip()
-                    img_format = img_dimensions[1].strip()
-                    img_url = json.loads(img_item["m"])
 
-                    img_dict['url'] = img_url["murl"]
-                    img_dict['title'] = img_name
-                    img_dict['width'] = img_width
-                    img_dict['height'] = img_height
-                    # img_format = img_format
-                    img_dict['format'] = img_format
-                except KeyError:
-                    pass
-            if img_url != "null":
-                imgs.append(ImageItem(img_dict))
-        return imgs
-            
         
 if __name__ == "__main__":
     api = ImageApi(5)
